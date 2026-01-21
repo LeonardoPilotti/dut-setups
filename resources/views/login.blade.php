619dedd668ad86ed59f461172e37576167001c89
LeonardoPilotti <pilottileonardo3@gmail.com>
2026-01-21 16:47:17 -0300

    feat: (Login) improve ui and add errors

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,19 +1,39 @@
 <x-layout>
-<main>
-    <form action="{{ route('auth.login') }}"
-        method="POST">
-        @csrf
-            
-        <label for="email">Email:</label>
-        <input
-            type="email"
-            name="email"
-            placeholder="your@email.com">
+    <main class="min-h-screen flex items-center justify-center">
+        <section class="bg-white w-full max-w-md p-8 shadow-lg rounded-xl">
+            <h1 class="text-center text-2xl font-bold mb-6">Login</h1>
+            <p>Faça login e garanta os melhores setups para o F1 25!</p>
+            <div>
+            <form action="{{ route('auth.login') }}" method="POST" class="flex flex-col gap-4">
+                @csrf
+                    <label for="email">
+                        Email:
+                    </label>
 
-        <label for="password">Senha:</label>
-        <input type="password" name="password" id="password">
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="your@email.com"
+                        class="border rounded-lg p-2">
+                
 
-        <button type="submit">Entrar</button>
-    </form>
-</main>
+                <label
+                    for="password">
+                    Senha:
+                    </label>
+
+                <input
+                    type="password"
+                    name="password"
+                    class="border rounded-lg p-2">
+
+                <button
+                    type="submit"
+                    class="bg-[#612a52] text-white p-2 rounded-lg font-bold cursor-pointer hover:bg-[#7b3b6a] mt-4">
+                    Entrar
+                </button>
+                </div>
+            </form>
+        </section>
+    </main>
 </x-layout>
