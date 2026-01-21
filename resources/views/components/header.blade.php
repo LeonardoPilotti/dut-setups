b7494016930ded026e3cbec0aa3d717b3bed94d1
LeonardoPilotti <pilottileonardo3@gmail.com>
2026-01-21 16:15:24 -0300

    feat: add logo button to home

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,11 +1,7 @@
 <header class="bg-[#2e1e45]">
     <div class="ml-12 p-4 flex items-center gap-3">
-        <img 
-            src="{{ asset('images/dutLogo.png') }}" 
-            alt="DUT Racing"
-            class="h-8 w-auto"
-        />
-
+        <a href="{{ route('site.home') }}"><img src="{{ asset('images/dutLogo.png') }}" alt="DUT Racing"
+                class="h-8 w-auto" /></a>
         <p class="text-white font-bold">
             DUT Racing
         </p>
